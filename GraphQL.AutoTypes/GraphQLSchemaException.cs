543912dfc48d325b5ab47d7478c7752decd0ed46
Alessandro Lazzara <fenomeno_8_3@hotmail.com>
2020-08-09 14:46:16 +0200

    first commit

---
+++ GraphQL.AutoTypes/GraphQLSchemaException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphQL.AutoTypes
+{
+    public class GraphQLSchemaException : ApplicationException
+    {
+        string _genericTypeName = null;
+
+        public GraphQLSchemaException() : base() { }
+        public GraphQLSchemaException(string genericTypeName) : base() { }
+        public GraphQLSchemaException(string genericTypeName, string message) : base(message: message) { }
+        public GraphQLSchemaException(string genericTypeName, string message, Exception innerException) : base(message: message, innerException: innerException) { }
+
+        public string GenericTypeName => _genericTypeName;
+    }
+}
